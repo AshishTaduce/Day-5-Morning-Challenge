27b361a08a4b7c9a43fb79af458f1747f60e98aa
AshishTaduce <ashishteddy2@gmail.com>
2019-10-04 15:31:42 +0530

    Done

--- bin/main.dart
+++ bin/main.dart
@@ -6,6 +6,25 @@
 //    [3, 3, 3],
 //    [3, 3, 3]
 //  ]
+
+List squarePatch(int size) {
+  List row = [];
+  for (int i = 0; i < size; i++) {
+    List temp = [];
+    for (int i = 0; i < size; i++){
+      temp.add(size);
+    }
+    row.add(temp);
+    }
+  for (var x in row){
+    print (x);
+  }
+  }
+
+
+
+
+
 //
 //squarePatch(5) should print
 //  [
@@ -24,13 +43,14 @@
 // squarePatch(0) should print []
 
 main() {
-
   List<List<String>> board = [
     ['O', ' ', ' '],
     [' ', 'X', ' '],
     ['X', ' ', ' ']
   ];
-
+  squarePatch(1);
+  squarePatch(5);
+  //printBoard(board);
   printBoard(board);
   // The above call should print:
   //    A   B   C
@@ -42,6 +62,7 @@ main() {
 
   board[0][2] = 'O';
   printBoard(board);
+  //printBoard(board);
   // The above call should print:
   //    A   B   C
   // 1  O |   | O
@@ -54,3 +75,12 @@ main() {
 // Write a function called 'printBoard' which takes a List of List of Strings,
 // and prints a Tic Tac Toe board to the console.  The board will always
 // have dimensions 3 x 3.
+
+void printBoard(List x) {
+  print('   A   B   C');
+  print('1  ${x[0][0]} | ${x[0][1]} | ${x[0][2]}');
+  print('  ---+---+---');
+  print('2 ${x[1][0]}  | ${x[1][1]} |${x[1][2]}');
+  print('  ---+---+---');
+  print('3  ${x[2][0]} | ${x[2][1]} |${x[2][2]}');
+}
